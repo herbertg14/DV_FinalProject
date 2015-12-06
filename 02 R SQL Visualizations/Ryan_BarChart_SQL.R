2b67e50a606d7599a4d141ee7e22c1df1f6b22af
Ryan Wechter <ryan.wechter@utexas.edu>
2015-12-06 17:16:11 -0600

    Finished Workflow

---
+++ 02 R SQL Visualizations/Ryan_BarChart_SQL.R
@@ -0,0 +1,2 @@
+INFLATION <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from INFLATION"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
