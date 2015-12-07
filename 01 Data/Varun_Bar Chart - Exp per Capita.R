f1f791d86d015652ed406d019e6ca5eed1f7a38d
Varun Parameswaran <vparam15@gmail.com>
2015-12-07 10:44:59 -0600

    Added a lot to the Rmd

--- 01 Data/Varun_Bar Chart - Exp per Capita.R
+++ 01 Data/Varun_Bar Chart - Exp per Capita.R
@@ -1,3 +1,7 @@
+NHCE <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from NHCE"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hog74', PASS='orcl_hog74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+state_pop <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from state_pop"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_vp5467', PASS='orcl_vp5467', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
 df1 <- NHCE %>% select(STATE_NAME, Y2009, REGION_NAME, CODE) %>% filter(STATE_NAME != 'null', CODE != '12') %>% group_by(STATE_NAME, REGION_NAME) %>% summarize(Y2009 = sum(Y2009)) %>% rename(STATE = STATE_NAME)
 
 df2 <- state_pop %>% select(STATE, NINE) %>% filter(STATE %in% c('Source: ', 'Puerto Rico') == FALSE, NINE != 'null')
--- 01 Data/Varun_Crosstab - Average Exp Growth.R
+++ 01 Data/Varun_Crosstab - Average Exp Growth.R
@@ -1,5 +1,6 @@
-df1 <- NHCE %>% select(CODE, ITEM,STATE_NAME,AVERAGE_ANNUAL_PERCENT_GROWTH) %>% filter(ITEM != 'Total State Spending',STATE_NAME != 'null') %>% mutate(LOW = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 7.01) %>% mutate(MED = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 10) %>% mutate(HIGH = LOW + MED)  
+NHCE <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from NHCE"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hog74', PASS='orcl_hog74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
+df1 <- NHCE %>% select(ITEM,STATE_NAME,AVERAGE_ANNUAL_PERCENT_GROWTH) %>% filter(ITEM != 'Total State Spending',STATE_NAME != 'null') %>% mutate(LOW = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 7.01) %>% mutate(MED = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 10) %>% mutate(HIGH = LOW + MED)  
 
 df1 %>% ggplot(aes(x=STATE_NAME,y=ITEM,label=round(AVERAGE_ANNUAL_PERCENT_GROWTH,digits = 2),color = factor(HIGH))) + geom_text() + labs(title = "Average Healthcare Expenditure Growth", x = "Item", y = "State") + scale_colour_manual(values = c("green", "blue", "red", "red"), labels = c("low", "medium", "high", "high")) + coord_flip()
 
