397f1fefca5fcf73be345148b528b967d24ba9e8
Ryan Wechter <ryan.wechter@utexas.edu>
2015-12-07 02:03:31 -0600

    We are done.

--- 02 R SQL Visualizations/Varun_Crosstab - Average Exp Growth.R
+++ 02 R SQL Visualizations/Varun_Crosstab - Average Exp Growth.R
@@ -1,8 +1,4 @@
-df1 <- NHCE %>% select(CODE, ITEM,STATE_NAME,AVERAGE_ANNUAL_PERCENT_GROWTH) %>% filter(ITEM != 'Total State Spending',STATE_NAME != 'null')
-
-df1 <- mutate(df1, LOW = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 7.01)
-df1 <- mutate(df1, MED = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 10)
-df1 <- mutate(df1, HIGH = LOW + MED)  
+df1 <- NHCE %>% select(CODE, ITEM,STATE_NAME,AVERAGE_ANNUAL_PERCENT_GROWTH) %>% filter(ITEM != 'Total State Spending',STATE_NAME != 'null') %>% mutate(LOW = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 7.01) %>% mutate(MED = (AVERAGE_ANNUAL_PERCENT_GROWTH) %/% 10) %>% mutate(HIGH = LOW + MED)  
 
 
 df1 %>% ggplot(aes(x=STATE_NAME,y=ITEM,label=round(AVERAGE_ANNUAL_PERCENT_GROWTH,digits = 2),color = factor(HIGH))) + geom_text() + labs(title = "Average Healthcare Expenditure Growth", x = "Item", y = "State") + scale_colour_manual(values = c("green", "blue", "red", "red"), labels = c("low", "medium", "high", "high")) + coord_flip()
